eb2640d8359f6e9b025dd5be720c5022f28ffbe0
Konstantin <mikole@bk.ru>
2022-06-01 14:51:13 +0800

    view

--- resources/views/asset/show.blade.php
+++ resources/views/asset/show.blade.php
@@ -71,8 +71,8 @@
                 </div>
 
                 <div class="links">
-                    <a href="http://127.0.0.1:8000/locations">Locations</a>
-                    <a href="http://127.0.0.1:8000/workers">Workers</a>
+                    <a href="/locations">Locations</a>
+                    <a href="/workers">Workers</a>
                 </div>
             </div>
         </div>
